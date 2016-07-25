e2259944afc262a00333502ebd93516721cfa718
sharathMSk <sharatk@microsoft.com>
2016-07-25 12:34:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.port || 1337
 
 app.get('/', function (req, res) {
-    res.send('Hello World! Welcome Sharath\n How you doing');
+    res.send('Hello World! Welcome Sharath. This is Lord. How you doing?');
 });
 
 var server = app.listen(port, function () {
